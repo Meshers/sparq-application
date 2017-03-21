19369853dfb63977f53788157a335c8f93ed6f15
aphasingnirvana <pranavsinghania08@gmail.com>
2017-03-21 09:11:21 +0530

    -

--- app/src/main/java/com/sparq/application/layer/ApplicationLayerManager.java
+++ app/src/main/java/com/sparq/application/layer/ApplicationLayerManager.java
@@ -1,6 +1,12 @@
 package com.sparq.application.layer;
 
+import com.sparq.application.layer.pdu.ApplicationLayerPdu;
+
 import java.io.UnsupportedEncodingException;
+
+import test.com.blootoothtester.network.linklayer.LinkLayerManager;
+import test.com.blootoothtester.network.linklayer.LlMessage;
+import test.com.blootoothtester.util.Logger;
 
 /**
  * This class contains information about the current context in which the LinkLayer is running such
@@ -9,13 +15,30 @@ import java.io.UnsupportedEncodingException;
 
 public class ApplicationLayerManager {
     private AlContext mAlContext;
+    private LinkLayerManager mLinkLayerManager;
+    private Logger mLogger = new Logger();
 
-    public ApplicationLayerManager(){
+    public ApplicationLayerManager(LinkLayerManager linkLayerManager){
         this.mAlContext = new AlContext();
+        mLinkLayerManager = linkLayerManager;
+
+        //I'm sorry
+        AlContext.Callback callback = new AlContext.Callback() {
+            @Override
+            public void transmitPdu(ApplicationLayerPdu pdu) {
+                //mLogger.d("ApplicationLayerManager", "sendData: seq.id: " + pdu.getSequenceId());
+                //mLinkLayerManager.sendData();
+            }
+
+            @Override
+            public void sendUpperLayer(AlMessage message) {
+                //mLinkLayerManager.send(message);
+            }
+        }
     }
 
-    public void sendData(byte[] packet, byte toAddr) {
-        mAlContext.sendPdu(toAddr, packet);
+    public void sendData(byte[] packet, byte type) {
+        mAlContext.sendPdu(type, packet);
     }
 
     public void sendData(String msg, byte toAddr) {
