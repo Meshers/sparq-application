aafc9d2372706545416e611e30475ebc92636988
aphasingnirvana <pranavsinghania08@gmail.com>
2017-03-22 01:10:54 +0530

    -

--- app/src/main/java/com/sparq/application/layer/ApplicationLayerManager.java
+++ app/src/main/java/com/sparq/application/layer/ApplicationLayerManager.java
@@ -1,11 +1,16 @@
 package com.sparq.application.layer;
 
+import android.content.BroadcastReceiver;
+
+import com.sparq.application.layer.almessage.AlMessage;
 import com.sparq.application.layer.pdu.ApplicationLayerPdu;
 
 import java.io.UnsupportedEncodingException;
 
+import test.com.blootoothtester.bluetooth.MyBluetoothAdapter;
+import test.com.blootoothtester.network.linklayer.DeviceDiscoveryHandler;
 import test.com.blootoothtester.network.linklayer.LinkLayerManager;
-import test.com.blootoothtester.network.linklayer.LlMessage;
+import test.com.blootoothtester.network.linklayer.LlContext;
 import test.com.blootoothtester.util.Logger;
 
 /**
@@ -14,29 +19,38 @@ import test.com.blootoothtester.util.Logger;
  */
 
 public class ApplicationLayerManager {
+    private MyBluetoothAdapter mBluetoothAdapter;
+    private BroadcastReceiver mBroadcastReceiver;
+    private DeviceDiscoveryHandler mDiscoveryHandler;
     private AlContext mAlContext;
     private LinkLayerManager mLinkLayerManager;
     private Logger mLogger = new Logger();
+    private LlContext mLlContext;
 
-    public ApplicationLayerManager(LinkLayerManager linkLayerManager){
-        this.mAlContext = new AlContext();
+    public ApplicationLayerManager(LinkLayerManager linkLayerManager, MyBluetoothAdapter bluetoothAdapter,
+                                   DeviceDiscoveryHandler discoveryHandler){
         mLinkLayerManager = linkLayerManager;
+        mBluetoothAdapter = bluetoothAdapter;
+        mDiscoveryHandler = discoveryHandler;
 
         //I'm sorry
         AlContext.Callback callback = new AlContext.Callback() {
             @Override
             public void transmitPdu(ApplicationLayerPdu pdu) {
-                //mLogger.d("ApplicationLayerManager", "sendData: seq.id: " + pdu.getSequenceId());
-                //mLinkLayerManager.sendData();
+                // FIXME: 22/3/17
+                // mLogger.d("ApplicationLayerManager", "sendData: seq.id: " + pdu.getSequenceId());
+                // mLinkLayerManager.sendData();
             }
 
             @Override
+            // FIXME: 22/3/17 Need to decide how to handle AlMessage
             public void sendUpperLayer(AlMessage message) {
-                //mLinkLayerManager.send(message);
+                mDiscoveryHandler.handleDiscovery(message);
             }
         }
     }
 
+    // FIXME: 22/3/17 when AlContext is built
     public void sendData(byte[] packet, byte type) {
         mAlContext.sendPdu(type, packet);
     }
@@ -47,6 +61,15 @@ public class ApplicationLayerManager {
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+    }
+
+    public void sendMessageToLinkLayer(String msg, Byte toId) {
+        mLinkLayerManager.sendData(msg, toId);
+    }
+
+    public String getMessageFromLinkLayer(){
+        
+        //Don't know how to use that callback
     }
 
 }
